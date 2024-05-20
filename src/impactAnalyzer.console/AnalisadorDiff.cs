0c8c322eac5e6ffe3b21993eacc0b5cc5845060c
Jeison O. da Silva <jeison.desenvolvimento@gmail.com>
2024-05-20 00:09:12 -0300

    feat: criado fluxo para identificar referencias

--- src/impactAnalyzer.console/AnalisadorDiff.cs
+++ src/impactAnalyzer.console/AnalisadorDiff.cs
@@ -1,23 +1,43 @@
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.FindSymbols;
 
 namespace impactAnalyzer.console
 {
     public class AnalisadorDiff
     {
-        public async Task AnalisarMetodo(List<Document> documents)
+        public async Task AnalisarMetodo(List<Document> documents, Solution solution)
         {
             foreach (var item in documents)
             {
                 try
                 {
+                    var semanticModel = await item.GetSemanticModelAsync();
                     var syntaxTree = await item.GetSyntaxTreeAsync();
                     var root = await syntaxTree.GetRootAsync();
 
                     var metodos = root.DescendantNodes().OfType<MethodDeclarationSyntax>();
                     foreach (var method in metodos )
                     {
-                        Console.WriteLine($"Method: {method.Identifier}");
+                        var methodSymbol = semanticModel.GetDeclaredSymbol(method);
+                        if (methodSymbol != null){
+                            var references = await SymbolFinder.FindReferencesAsync(methodSymbol, solution);
+
+                            foreach (var reference in references)
+                            {
+                                foreach (var location in reference.Locations)
+                                {
+                                    var referenceLocation = location.Location;
+
+                                    // Obter a linha e posição da referência
+                                    var lineSpan = referenceLocation.GetLineSpan();
+                                    var lineNumber = lineSpan.StartLinePosition.Line;
+                                    var charPosition = lineSpan.StartLinePosition.Character;
+
+                                    Console.WriteLine($"  Referenced at: {referenceLocation.SourceTree.FilePath} (Line {lineNumber + 1}, Char {charPosition + 1})");
+                                }
+                            }
+                        }
                     }
                 }
                 catch (System.Exception ex)
