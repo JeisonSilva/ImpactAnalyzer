59149d9eefbef5e760dbe3649f8fd84adaeb43d6
Jeison O. da Silva <jeison.desenvolvimento@gmail.com>
2024-05-20 00:20:48 -0300

    feat: Ajuste na exibicao de referencias

--- src/impactAnalyzer.console/AnalisadorDiff.cs
+++ src/impactAnalyzer.console/AnalisadorDiff.cs
@@ -20,35 +20,40 @@ namespace impactAnalyzer.console
                     foreach (var method in metodos )
                     {
                         var methodSymbol = semanticModel.GetDeclaredSymbol(method);
-                        if (methodSymbol != null){
+                        if (methodSymbol != null)
+                        {
                             var references = await SymbolFinder.FindReferencesAsync(methodSymbol, solution);
-                            
+
                             var methodText = method.ToFullString();
                             Console.WriteLine($"Method: {method.Identifier.Text}");
                             Console.WriteLine("Code:");
                             Console.WriteLine(methodText);
                             Console.WriteLine();
-
-                            foreach (var reference in references)
-                            {
-                                foreach (var location in reference.Locations)
-                                {
-                                    var referenceLocation = location.Location;
-
-                                    // Obter a linha e posição da referência
-                                    var lineSpan = referenceLocation.GetLineSpan();
-                                    var lineNumber = lineSpan.StartLinePosition.Line;
-                                    var charPosition = lineSpan.StartLinePosition.Character;
-
-                                    Console.WriteLine($"  Referenced at: {referenceLocation.SourceTree.FilePath} (Line {lineNumber + 1}, Char {charPosition + 1})");
-                                }
-                            }
+                            ExibirReferencias(references);
                         }
                     }
                 }
                 catch (System.Exception ex)
                 {
                     Console.WriteLine($"Error: {ex.Message}");
+                }
+            }
+        }
+
+        private static void ExibirReferencias(IEnumerable<ReferencedSymbol> references)
+        {
+            foreach (var reference in references)
+            {
+                foreach (var location in reference.Locations)
+                {
+                    var referenceLocation = location.Location;
+
+                    // Obter a linha e posição da referência
+                    var lineSpan = referenceLocation.GetLineSpan();
+                    var lineNumber = lineSpan.StartLinePosition.Line;
+                    var charPosition = lineSpan.StartLinePosition.Character;
+
+                    Console.WriteLine($"  Referenced at: {referenceLocation.SourceTree.FilePath} (Line {lineNumber + 1}, Char {charPosition + 1})");
                 }
             }
         }
