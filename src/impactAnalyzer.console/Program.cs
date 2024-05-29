a1aa384e5998f455af631b9478c446487827cbce
Jeison O. da Silva <jeison.desenvolvimento@gmail.com>
2024-05-29 00:03:23 -0300

    feat: Criado primeira versao do analisador

--- src/impactAnalyzer.console/Program.cs
+++ src/impactAnalyzer.console/Program.cs
@@ -1,70 +1,16 @@
-﻿
-
-using impactAnalyzer.console;
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.MSBuild;
-
-var solutionPath = @"/home/jeison/Documentos/projetos/Moq.AutoMocker/Moq.AutoMock.sln";
-var methodToFind = "CreateInstance";
-
-using var workspace = MSBuildWorkspace.Create();
-var solution = await workspace.OpenSolutionAsync(solutionPath);
-
-var methodCalls = new Dictionary<string, LinkedList<string>>();
-
-await AnalisarMetodos(methodToFind, solution, methodCalls);
+﻿using impactAnalyzer.models;
 
 
 
+var analisador = new AnalisadorProjeto();
+var metodo = new Metodo(args);
+var grupoProjeto = new GrupoProjeto(args);
 
-foreach (var kvp in methodCalls)
+var result = await analisador.Analisar(metodo, grupoProjeto);
+
+foreach (var kvp in result)
 {
     Console.WriteLine($"Método '{kvp.Key}' é chamado pelos seguintes métodos:");
     Console.WriteLine(string.Join(", ", kvp.Value));
     
-}
-
-
-//var processador = new ProcessadorMudanca(new AnalisadorDiff());
-//await processador.ProcessarMudancas();
-
-static async Task AnalisarMetodos(string methodToFind, Solution solution, Dictionary<string, LinkedList<string>> methodCalls)
-{
-    foreach (var project in solution.Projects)
-    {
-        foreach (var document in project.Documents)
-        {
-            var syntaxTree = await document.GetSyntaxTreeAsync();
-            var root = await syntaxTree.GetRootAsync();
-
-            var semanticModel = await document.GetSemanticModelAsync();
-
-            var methods = root.DescendantNodes().OfType<MethodDeclarationSyntax>();
-            foreach (var method in methods)
-            {
-                var methodName = method.Identifier.ValueText;
-                var calls = method.DescendantNodes().OfType<InvocationExpressionSyntax>();
-
-                foreach (var call in calls)
-                {
-                    var symbol = semanticModel.GetSymbolInfo(call).Symbol as IMethodSymbol;
-                    if (symbol != null && symbol.Name == methodToFind)
-                    {
-                        if (!methodCalls.ContainsKey(methodToFind))
-                        {
-                            methodCalls[methodToFind] = new LinkedList<string>();
-                        }
-
-                        if(!methodCalls[methodToFind].Contains(methodName) && methodToFind != methodName){
-                            methodCalls[methodToFind].AddFirst(methodName);
-                            await AnalisarMetodos(methodName, solution, methodCalls);
-                        }
-
-                        
-                    }
-                }
-            }
-        }
-    }
 }
